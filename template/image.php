e91853be507dd14a29294265d811d18d6d26ee4a
Pascal Hertleif <killercup@gmail.com>
2011-03-14 21:57:36 +0100

    added some jquery fun - click on image to show in original size - click on share to show and auto-select embed code

--- template/image.php
+++ template/image.php
@@ -4,10 +4,26 @@
 	<meta charset="UTF-8">
 	<title><? echo $filename ?></title>
 	<link rel="stylesheet" type="text/css" href="/template/style.css" media="all" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.1.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$("#image").click(function(){
+			if ( $(this).css("max-width") != "auto" ) {
+				$(this).css("max-width", "none");
+			} else {
+				$(this).css("max-width", "90%");
+			}
+		});
+		$("#share").click(function(){
+			$(this).addClass("vis");
+			$("#share input").select().focus();
+		});
+	});
+	</script>
 </head>
 <body>
 	
-	<img src="<? echo $filepath ?>" alt="<? echo $filename ?>" />
+	<img src="<? echo $filepath ?>" alt="<? echo $filename ?>" id="image" />
 	
 	<h1><a href="<?php echo $_SERVER["REQUEST_URI"] ?>"><? echo $filename ?></a></h1>
 	
